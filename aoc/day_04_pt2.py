44fed66314d803cbb52191dc3e1a032d63cf5f0d
Felipe Bidu Rodrigues <felipe@felipevr.com>
2023-03-28 14:55:53 -0300

    Update from old computer

--- aoc/day_04_pt2.py
+++ aoc/day_04_pt2.py
@@ -6,6 +6,8 @@ Day 0
 import cProfile
 import click
 
+import re
+
 strategies = []
 
 
@@ -37,17 +39,69 @@ def brute_force(lines):
     present on the passport using Python's `in` operator.
     """
 
+    def parse_fields(passport):
+        pattern = r"([a-z]{3}):([#|\w]*)"
+
+        passport = dict(re.findall(pattern, passport))
+
+        try:
+            passport["byr"] = int(passport["byr"])
+            passport["iyr"] = int(passport["iyr"])
+            passport["eyr"] = int(passport["eyr"])
+        except (KeyError, ValueError):
+            return {}
+
+        return passport
+
+    def valid_numeric(passport, field, min_, max_):
+        return min_ <= passport[field] <= max_
+
+    def valid_height(passport):
+        pattern = r"(\d+)(cm|in)"
+
+        try:
+            value, unit = re.findall(pattern, passport["hgt"])[0]
+            value = int(value)
+        except (IndexError, ValueError):
+            return False
+
+        if unit not in {"cm", "in"}:
+            return False
+
+        if unit == "cm":
+            return 150 <= value <= 193
+
+        return 59 <= value <= 76
+
+    def valid_hair_color(passport):
+        pattern = r"^#[a-f0-9]{6}$"
+        return bool(re.match(pattern, passport["hcl"]))
+
+    def valid_pid(passport):
+        pattern = r"^[0-9]{9}$"
+        return bool(re.match(pattern, passport["pid"]))
+
+    def valid_eye_color(passport):
+        return passport["ecl"] in {"amb", "blu", "brn", "gry", "grn", "hzl", "oth"}
+
     def is_valid_passport(passport_lines):
         passport = " ".join(passport_lines)
 
+        fields = {"byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"}
+
+        if not all(field in passport for field in fields):
+            return False
+
+        passport = parse_fields(passport)
+
         return (
-            "byr" in passport
-            and "iyr" in passport
-            and "eyr" in passport
-            and "hgt" in passport
-            and "hcl" in passport
-            and "ecl" in passport
-            and "pid" in passport
+            valid_numeric(passport, "byr", 1920, 2002)
+            and valid_numeric(passport, "iyr", 2010, 2020)
+            and valid_numeric(passport, "eyr", 2020, 2030)
+            and valid_height(passport)
+            and valid_hair_color(passport)
+            and valid_eye_color(passport)
+            and valid_pid(passport)
         )
 
     current_passport = []
